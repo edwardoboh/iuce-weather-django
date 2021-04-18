9626844dae347b540e54f78b2437088c6a40e38e
edwardoboh <osaretinedward@gmail.com>
2021-04-19 00:10:47 +0100

    change post request to get

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,20 @@
-from django.urls import path, include
-from rest_framework import routers
-from .views import SensorViewSet
+# from django.urls import path, include
+# from rest_framework import routers
+# from .views import SensorViewSet
 
-router = routers.DefaultRouter()
-router.register(r'sensor', SensorViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'sensor', SensorViewSet)
+
+# urlpatterns = [
+#     path("", include(router.urls))
+# ]
+
+from django.urls import path, include
+from .views import SensorView
+from .views import WeatherView
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("sensor/", SensorView.as_view()),
+    path("weather/", WeatherView.as_view()),
+    path("weather/<int:weather>/", WeatherView.as_view())
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,10 +1,44 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
-from rest_framework import viewsets
+# # Create your views here.
+# from rest_framework import viewsets
+# from .models import SensorModel
+# from .serializers import SensorSerializer
+
+# class SensorViewSet(viewsets.ModelViewSet):
+#     queryset = SensorModel.objects.all().order_by('-created')
+#     serializer_class = SensorSerializer
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .models import SensorModel
 from .serializers import SensorSerializer
 
-class SensorViewSet(viewsets.ModelViewSet):
-    queryset = SensorModel.objects.all().order_by('-created')
-    serializer_class = SensorSerializer
+class SensorView(APIView):
+    def get(self, request):
+        theModel = SensorModel.objects.all().order_by('-created')
+        serializedData = SensorSerializer(theModel, many=True)
+        return Response(serializedData.data)
+
+
+class WeatherView(APIView):
+    def post(self, request):
+            serializedData = SensorSerializer(data=request.data)
+            if serializedData.is_valid(raise_exception=True):
+                serializedData.save()
+                return Response(serializedData.data)
+            return Response(serializedData.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, weather):
+        weatherData = {
+            'temperature': weather.GET.getlist('temperature')[0],
+            'pressure': weather.GET.getlist('pressure')[0],
+            'humidity': weather.GET.getlist('humidity')[0],
+            'light': weather.GET.getlist('light')[0]
+        }
+        serializedWeather = SensorSerializer(data=weatherData)
+        if serializedWeather.is_valid():
+            serializedWeather.save()
+            return Response(serializedWeather.data)
+        return Response(serializedWeather.errors, status=status.HTTP_400_BAD_REQUEST)
